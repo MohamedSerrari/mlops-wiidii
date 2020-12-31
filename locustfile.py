78e80afeaa33c6ef50892d08c3e11ecea2b6db33
Tarek LAMMOUCHI <csj0oe@gmail.com>
2020-12-31 14:48:33 +0100

    test performance using sentences from test set

--- locustfile.py
+++ locustfile.py
@@ -1,17 +1,14 @@
 import urllib
+import json
+import random
 from locust import HttpUser, task
 
 class IntentionTestUser(HttpUser):
-    sentences = [
-        {"sentence": ""},
-        {"sentence": "restaurant"},
-        {"sentence": "j\'ai besoin d'un restaurant italien pour ce soir"},
-        {"sentence": "j'ai besoin de la météo pour demain"},
-        {"sentence": "j'ai besoin d'un hotel prés de moi"},
-        {"sentence": "j'ai besoin d'un smartphone pour un prix inférieur à 1000 euro"}
-    ]
     @task
     def intent_api(self):
-        for sentence in self.sentences:
+        with open("./data/testing_set.json") as file:
+            sentences = json.load(file)
+        for _ in range(0, 10):
+            sentence = random.choice(sentences)
             url = "/api/intent/?{}".format(urllib.parse.urlencode(sentence))
             self.client.get(url)
